8b032d59ab387632295ffeb3d7af5625668ffea7
Ismail <forwork.ismail@gmail.com>
2022-06-26 21:26:03 +0430

    Initial commit

---
+++ src/test/kotlin/com/forworkismail/kotlinspringboot/KotlinspringbootApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.forworkismail.kotlinspringboot
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class KotlinspringbootApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
